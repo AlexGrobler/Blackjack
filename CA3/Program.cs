285bb479c0a786165a3cfee2c528ca54cbdb9309
space-commits <57108281+space-commits@users.noreply.github.com>
2023-12-18 19:17:03 +0000

    Add project files.

---
+++ CA3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //display user score
+            //prompt user for playing a game
+            //clear console
+            //select 2 card objects from array of all posible cards at random
+            //display card value
+            //ask user if they want to receive another card or stop (stick or twist)
+
+            //if twist loop over adding another card then ask if they want to s/t
+            
+            //every time player receives another card, check if value is over 21 (they lose if so)
+
+            //if the player hasn't lost, and choose stick, then deal dealer's cards
+            //dealer must keep drawing until they get at least 17 (dealer only stops once score is 17 or higher or bust)
+            //
+
+        }
+    }
+}
